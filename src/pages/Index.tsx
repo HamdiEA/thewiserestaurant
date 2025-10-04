0516713c161503b6b662c211d91aa22a3a6ef6fe
HamdiEA <hamdielabedtn1@gmail.com>
2025-10-04 21:34:55 +0200

    Add files via upload

---
+++ src/pages/Index.tsx
@@ -0,0 +1,17 @@
+import Header from "@/components/Header";
+import HeroSection from "@/components/HeroSection";
+import MenuSection from "@/components/MenuSection";
+import Footer from "@/components/Footer";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-background">
+      <Header />
+      <HeroSection />
+      <MenuSection />
+      <Footer />
+    </div>
+  );
+};
+
+export default Index;
